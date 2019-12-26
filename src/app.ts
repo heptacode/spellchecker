b8072ca02cb2e3b13950cf4ab8b74bef157caede
heptacode <admin@hyunwoo.kim>
2019-12-26 14:17:41 +0900

    Update App

--- src/app.ts
+++ src/app.ts
@@ -17,12 +17,28 @@ const app: express.Application = express();
 
 app.use(cors());
 app.use(helmet());
-app.use(morgan("combined"));
+app.use(morgan("dev"));
 app.use(compression());
 
 app.set("trust proxy", true);
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(express.json({ limit: "50mb" }));
+
+let tsp: any = moment(new Date()).format("x");
+
+app.get("/", (req, res) => {
+	res.send("/");
+});
+
+app.post("/", (req, res) => {
+	tsp = moment(new Date()).format("x");
+	axios
+		.get(`https://m.search.naver.com/p/csearch/ocontent/util/SpellerProxy?_callback=jQuery1124013638432540936685_${tsp}&q=${encodeURI(req.body.origin)}&where=nexearch&color_blindness=0&_=${tsp}`)
+		.then(data => {
+			console.log(data.data);
+			res.send(data.data);
+		});
+});
 
 app.listen(process.env.HTTP_PORT || 80, () => {
 	Log.i(`Listening on http://${process.env.HTTP_HOST || "localhost"}:${process.env.HTTP_PORT || 80}`);
